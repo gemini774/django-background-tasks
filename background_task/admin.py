e2c9b14d9aed866d0b8972600474ffc9fe34789c
Mominur <mominurrohman774@gmail.com>
2023-12-31 20:12:15 +0600

    hide models from admin

--- background_task/admin.py
+++ background_task/admin.py
@@ -16,17 +16,17 @@ def dec_priority(modeladmin, request, queryset):
         obj.save()
 dec_priority.short_description = "priority -= 1"
 
-class TaskAdmin(admin.ModelAdmin):
-    display_filter = ['task_name']
-    search_fields = ['task_name', 'task_params', ]
-    list_display = ['task_name', 'task_params', 'run_at', 'priority', 'attempts', 'has_error', 'locked_by', 'locked_by_pid_running', ]
-    actions = [inc_priority, dec_priority]
+# class TaskAdmin(admin.ModelAdmin):
+#     display_filter = ['task_name']
+#     search_fields = ['task_name', 'task_params', ]
+#     list_display = ['task_name', 'task_params', 'run_at', 'priority', 'attempts', 'has_error', 'locked_by', 'locked_by_pid_running', ]
+#     actions = [inc_priority, dec_priority]
 
-class CompletedTaskAdmin(admin.ModelAdmin):
-    display_filter = ['task_name']
-    search_fields = ['task_name', 'task_params', ]
-    list_display = ['task_name', 'task_params', 'run_at', 'priority', 'attempts', 'has_error', 'locked_by', 'locked_by_pid_running', ]
+# class CompletedTaskAdmin(admin.ModelAdmin):
+#     display_filter = ['task_name']
+#     search_fields = ['task_name', 'task_params', ]
+#     list_display = ['task_name', 'task_params', 'run_at', 'priority', 'attempts', 'has_error', 'locked_by', 'locked_by_pid_running', ]
 
 
-admin.site.register(Task, TaskAdmin)
-admin.site.register(CompletedTask, CompletedTaskAdmin)
+# admin.site.register(Task, TaskAdmin)
+# admin.site.register(CompletedTask, CompletedTaskAdmin)
